8f3449e0fb2b9a8548c0dddee4e55b55c7ddc0df
MrGigglesXD <andresarturo012@gmail.com>
2019-04-03 22:13:28 -0400

    ahora si

---
+++ Js/Funciones.js
@@ -0,0 +1,18 @@
+	$(document).ready(function()
+{
+
+	$("#ClickF").click(function(){
+    $("#FormContacto").slideToggle("slow");
+
+   								});
+	$(".programs, .movies, .op img").hover(function(){
+    $(this).css("border","2px dotted magenta");
+
+    $(".logo").hover(function(){
+    $(this).slideToggle("slow")
+		 
+    });
+	  });
+    
+	
+});
